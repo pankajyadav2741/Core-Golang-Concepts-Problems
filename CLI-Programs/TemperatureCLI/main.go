3d0a51e60e5747aaa557c355fbf59eb4e5efc96e
Pankaj Yadav <pankajyadav2741@gmail.com>
2021-08-15 14:16:28 +0530

    Update main.go

--- CLI-Programs/TemperatureCLI/main.go
+++ CLI-Programs/TemperatureCLI/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "module github.com/pankajyadav2741/Core-Golang-Concepts-Problems/CLI-Programs/TemperatureCLI/src"
+import "github.com/pankajyadav2741/Core-Golang-Concepts-Problems/CLI-Programs/TemperatureCLI/src"
 
 func main() {
 	src.StartApp()
